e996d6c69c35b1cad7fc712ef73135037a3088aa
Renan.T <rtoniolo4@gmail.com>
2023-05-12 12:40:28 -0300

    ajust in AnaliserNumber

--- vscode exer/AnaliseNumber/script.js
+++ vscode exer/AnaliseNumber/script.js
@@ -1,40 +1,50 @@
-var selec = document.querySelector('select')
-    var result = document.querySelector('#result')
-var list = []
-function add(){
-    let input = document.querySelector('input').value
-        if(input.length == 0){
-            alert('Insira um valor válido')
-        } else if( Number(input) > 100 || list.includes(Number(input))){
-            alert('Número não aceito')
-        } else{
-            list.push(Number(input))
-                let obj = document.createElement('option')
-            obj.text = `Valor ${input} adicionado.`
-        selec.appendChild(obj)
-    }
+var selec = document.querySelector("select");
+var result = document.querySelector("#result");
+var list = [];
+function add() {
+  let input = document.querySelector("input");
+  if (input.value.length == 0) {
+    alert("Insira um valor válido");
+  } else if (
+    Number(input.value) > 100 ||
+    list.includes(Number(input.value)) ||
+    Number(input.value) < 0
+  ) {
+    alert("Número não aceito");
+  } else {
+    let push = Number(input.value);
+    list.push(push);
+    let obj = document.createElement("option");
+    obj.text = `Valor ${input.value} adicionado.`;
+    selec.appendChild(obj);
+  }
+  input.value = "";
+  input.focus();
 }
-function somatodos(lista){
-    let soma = 0
-        let vari = 0
-        while(vari < lista.length){
-            soma += lista[vari]
-            vari++
-        }
-    return soma 
+function somatodos(lista) {
+  let soma = 0;
+  let vari = 0;
+  while (vari < lista.length) {
+    soma += lista[vari];
+    vari++;
+  }
+  return soma;
 }
-function finaly(){
-    result.style = 'padding:18px;'
-    let input = document.querySelector('input').value
-    if (input.length == 0){
-        alert('Insira um valor')
-    }else{
-        result.innerHTML = ''
-            list.sort()
-                result.innerHTML += `Foram cadastrados ${list.length} números.<br>`
-                    result.innerHTML += `O maior número cadastrado é ${list[list.length - 1]}.<br>`
-                result.innerHTML += `E o menor é ${list[0]}.<br>`
-            result.innerHTML += `A soma de todos são ${somatodos(list)}.<br>`
-        result.innerHTML += `E a média deles é ${(somatodos(list) / list.length).toFixed(1)}`
-    }
+function finaly() {
+  if (list.length == 0) {
+    alert("Insira um valor");
+  } else {
+    result.style = "padding:18px;";
+    result.innerHTML = "";
+    list.sort((a, b) => a - b);
+    result.innerHTML += `Foram cadastrados ${list.length} números.<br>`;
+    result.innerHTML += `O maior número cadastrado é ${
+      list[list.length - 1]
+    }.<br>`;
+    result.innerHTML += `E o menor é ${list[0]}.<br>`;
+    result.innerHTML += `A soma de todos são ${somatodos(list)}.<br>`;
+    result.innerHTML += `E a média deles é ${(
+      somatodos(list) / list.length
+    ).toFixed(1)}`;
+  }
 }
