63ebee90fa6146ae5d0c44010b2fafaf3b769d83
Julio Saito <saitodisse@gmail.com>
2023-05-12 21:30:50 -0300

    [calculadora] estilos css

--- vscode exer/Calculadora_1.0/script.js
+++ vscode exer/Calculadora_1.0/script.js
@@ -1,13 +1,13 @@
-function apagar(){
-    var sele = document.querySelector('div#div')
-    sele.innerText = ''  
+function apagar() {
+  var sele = document.querySelector("div#mostrador");
+  sele.innerText = "";
 }
-function addButt(id){
-    var selec = document.querySelector('div#div')
-    selec.innerText += id
+function addButt(id) {
+  var selec = document.querySelector("div#mostrador");
+  selec.innerText += id;
 }
-function resultado(){
-    var selecDiv = document.querySelector('div#div')
-    var resultado = eval(selecDiv.innerText)
-    selecDiv.innerText = resultado
+function resultado() {
+  var selecDiv = document.querySelector("div#mostrador");
+  var resultado = eval(selecDiv.innerText);
+  selecDiv.innerText = resultado;
 }
