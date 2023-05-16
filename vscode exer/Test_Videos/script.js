1c7f678859db2aff9f220c5aaa64741dab1af925
Renan.T <rtoniolo4@gmail.com>
2023-05-16 10:38:32 -0300

    delete code Videos_test

--- vscode exer/Test_Videos/script.js
+++
@@ -1,5 +0,0 @@
-var tb_ht = document.querySelectorAll("img");
-
-var video = document.querySelector("iframe");
-video.src =
-  "https://www.youtube.com/watch?v=gqrySQQzvvQ&list=PLHz_AreHm4dkZ9-atkcmcBaMZdmLHft8n&index=37";
