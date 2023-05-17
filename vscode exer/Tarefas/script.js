92ce49cb63669926c4923eee3c9c694674f58869
Renan.T <rtoniolo4@gmail.com>
2023-05-17 09:48:07 -0300

    uptade Tarefas (function checked) and ajust

--- vscode exer/Tarefas/script.js
+++ vscode exer/Tarefas/script.js
@@ -1,12 +1,13 @@
 const selet_in = document.querySelector("input");
 const selet_select = document.querySelector("select");
+
 function add() {
   let tarefa = document.createElement("option");
   if (selet_in.value.length == 0) {
     alert("Insira uma Tarefa válida");
   } else {
-    tarefa.text = selet_in.value;
-    tarefa.id = selet_in.value;
+    tarefa.text = "•" + selet_in.value;
+    tarefa.id = tarefa.text;
     selet_select.appendChild(tarefa);
     selet_in.focus();
     selet_in.value = "";
@@ -18,6 +19,11 @@ selet_in.addEventListener("keyup", function (event) {
   }
 });
 function remove() {
-  let selet_option = document.getElementById(selet_select.value);
+  let select = selet_select.value;
+  let selet_option = document.getElementById(select.split("✅")[0]);
   selet_select.removeChild(selet_option);
+}
+function checked() {
+  let selet_option = document.getElementById(selet_select.value);
+  selet_option.text += "✅";
 }
