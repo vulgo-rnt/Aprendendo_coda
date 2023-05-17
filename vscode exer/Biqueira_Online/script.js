98e7f5b7e6cd3b767cd35952eda0828fde613f3e
Renan.T <rtoniolo4@gmail.com>
2023-05-17 13:07:24 -0300

    uptade script

--- vscode exer/Biqueira_Online/script.js
+++ vscode exer/Biqueira_Online/script.js
@@ -1,73 +1,21 @@
-const selet_select = document.querySelector("select");
-const selet_result = document.getElementById("result");
+const select = document.querySelector("select");
+const result = document.getElementById("result");
+var lista = {};
 var value_all = 0;
-var maconha = 0;
-var po = 0;
-var k9 = 0;
-var lsd = 0;
-var bala = 0;
 function add() {
-  value_all += Number(selet_select.value);
-  var selet_label = selet_select[selet_select.selectedIndex].parentNode.label;
-  if (maconha == 0 && selet_label == "Maconha") {
+  let label = select[select.selectedIndex].parentNode.label;
+  value_all += Number(select[select.selectedIndex].value);
+  if (!lista[label]) {
     let div = document.createElement("div");
-    div.id = selet_label;
-    div.text = "1x Maconha";
-    selet_result.appendChild(div);
-    maconha++;
+    div.id = label;
+    div.innerText = `1x ${label}`;
+    result.appendChild(div);
+    lista[label] = 1;
+  } else {
+    lista[label]++;
+    let div = document.getElementById(label);
+    div.innerHTML = `${lista[label]}x ${label}`;
   }
-  if (selet_label == "Maconha" && maconha > 0) {
-    let div = document.getElementById(selet_label);
-    div.innerText = `${maconha}X Maconha`;
-    maconha++;
-  }
-  if (po == 0 && selet_label == "Pó") {
-    let div = document.createElement("div");
-    div.id = selet_label;
-    div.text = "1x Pó";
-    selet_result.appendChild(div);
-    div.innerHTML = "1x Pó";
-    po++;
-  }
-  if (selet_label == "Pó" && po > 0) {
-    let div = document.getElementById(selet_label);
-    div.innerHTML = `${po}x Pó`;
-    po++;
-  }
-  if (lsd == 0 && selet_label == "Lsd") {
-    let div = document.createElement("div");
-    div.id = selet_label;
-    div.text = "1x Lsd";
-    selet_result.appendChild(div);
-    lsd++;
-  }
-  if (selet_label == "Lsd" && lsd > 0) {
-    let div = document.getElementById(selet_label);
-    div.innerHTML = `${lsd}x Lsd`;
-    lsd++;
-  }
-  if (bala == 0 && selet_label == "Bala") {
-    let div = document.createElement("div");
-    div.id = selet_label;
-    div.text = "1x Bala";
-    selet_result.appendChild(div);
-    bala++;
-  }
-  if (selet_label == "Bala" && bala > 0) {
-    let div = document.getElementById(selet_label);
-    div.innerHTML = `${bala}x Bala`;
-    bala++;
-  }
-  if (k9 == 0 && selet_label == "K9") {
-    let div = document.createElement("div");
-    div.id = selet_label;
-    div.text = "1x K9";
-    selet_result.appendChild(div);
-    k9++;
-  }
-  if (selet_label == "K9" && k9 > 0) {
-    let div = document.getElementById(selet_label);
-    div.innerHTML = `${k9}x K9`;
-    k9++;
-  }
+  var value_result = document.getElementById("value_all");
+  value_result.innerHTML = `Valor Total da Compra : R$${value_all}`;
 }
