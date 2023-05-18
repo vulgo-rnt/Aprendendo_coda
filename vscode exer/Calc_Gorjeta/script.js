035d01ded4e657f4a55d7a19bcc9ddb410498f35
Renan.T <rtoniolo4@gmail.com>
2023-05-18 10:00:53 -0300

    uptade script Gorjeta

--- vscode exer/Calc_Gorjeta/script.js
+++ vscode exer/Calc_Gorjeta/script.js
@@ -1,2 +1,16 @@
 const input = document.querySelector("#value");
 const result = document.querySelector("#result");
+const list = [5, 15, 25];
+input.addEventListener("keyup", function (event) {
+  if (event.key == "Enter") {
+    for (let cont = 0; cont < list.length; cont++) {
+      if (document.getElementById(list[cont]).checked) {
+        result.innerHTML = `De R$${input.value} a gorjeta fica em R$${
+          (Number(input.value) * list[cont]) / 100
+        }`;
+      }
+    }
+    input.focus();
+    input.value = "";
+  }
+});
