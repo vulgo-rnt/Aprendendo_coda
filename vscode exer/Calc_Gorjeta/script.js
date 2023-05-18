17d72ab2f8da678e1252d642b97886e4a3f60537
Renan.T <rtoniolo4@gmail.com>
2023-05-18 13:30:10 -0300

    uptade css

--- vscode exer/Calc_Gorjeta/script.js
+++ vscode exer/Calc_Gorjeta/script.js
@@ -2,15 +2,20 @@ const input = document.querySelector("#value"); //Seletor do input tipo number
 const result = document.querySelector("#result"); //Seletor do paragrafo que vai mostra o texto com o resultado
 const radios = document.querySelectorAll("input[type='radio']"); //Seletor que pega todos os inputs tipo radio
 const list = [5, 15, 25]; //Lista com valores iguais o id dos radio, necessario para o loop for
-input.addEventListener("keyup", function (event) {
+input.addEventListener("keyup", (event) => {
   //Adiciona o evento keyup para usar o Enter em vez de usar 'click' com um button
   if (event.key == "Enter") {
-    for (let cont = 0; cont < list.length; cont++) {
-      if (document.getElementById(list[cont]).checked) {
-        //Checa cada input radio e muda o paragrafo para mostra o resultado
-        result.innerHTML = `De R$${input.value} a gorjeta fica em R$${
-          (Number(input.value) * list[cont]) / 100
-        }`;
+    if (input.value.length == 0) {
+      alert("Insira um valor");
+    } else {
+      for (let cont = 0; cont < list.length; cont++) {
+        if (document.getElementById(list[cont]).checked) {
+          //Checa cada input radio e muda o paragrafo para mostra o resultado
+          result.innerHTML = `De R$${input.value} a gorjeta fica em R$${
+            (Number(input.value) * list[cont]) / 100
+          }`;
+          result.style.padding = "10px"; //Muda o padding somente quando tiver imformação no paragrafo
+        }
       }
     }
     input.focus(); //Manter o foco no input tipo number depois de clickar Enter
@@ -23,4 +28,9 @@ radios.forEach((radio) => {
     //Adiciona um evento click para smp manter o foco no input tipo number
     input.focus();
   });
+});
+input.addEventListener("click", () => {
+  //Diminui o input quando selecionado para por um numero e tira o placeholder
+  input.style.width = "50px";
+  input.placeholder = "";
 });
