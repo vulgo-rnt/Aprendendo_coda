3580d5cc707834d508e815325443add122ff5812
Renan.T <rtoniolo4@gmail.com>
2023-05-27 16:45:36 -0300

    change logic code

--- vscode exer/Game/main.js
+++ vscode exer/Game/main.js
@@ -1,15 +1,5 @@
 const player = [];
 const boasEscolhas = ["bitcoin", "curso de ingles", "coda"];
-const todasEscolhas = [
-  ["fuma maconha", "curso de ingles", "namorar"],
-  ["coda", "bebidas mulheres", "lol games"],
-  ["", "", ""],
-];
-const textosPag = [
-  ["teste1", "teste1", "teste1"],
-  ["teste2", "teste2", "teste2"],
-  ["", "", ""],
-];
 const escolhas = document.querySelectorAll("button");
 let pag = 0;
 
@@ -27,21 +17,4 @@ function info_check(info) {
   let verif;
   info === boasEscolhas[pag] ? (verif = true) : (verif = false);
   player.push({ escolha: info, valor: verif });
-}
-
-function nextPag() {
-  if (pag > 1) {
-    if (verificar_valor(player)) {
-      escolhas[1].innerText = "final teste";
-    } else {
-      escolhas[0].innerText = "final teste";
-    }
-  }
-
-  for (let cont = 0; cont < 3; cont++) {
-    console.log(cont);
-    escolhas[cont].innerText = textosPag[pag][cont];
-    for (let c = 0; c <= 2; c++) console.log(todasEscolhas[cont][c]);
-  }
-  pag++;
 }
