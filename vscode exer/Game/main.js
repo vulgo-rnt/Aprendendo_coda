90ebb768e03e6690a343330700e65ecf6bf827bf
Renan.T <rtoniolo4@gmail.com>
2023-05-27 18:49:23 -0300

    adjusted logic

--- vscode exer/Game/main.js
+++ vscode exer/Game/main.js
@@ -2,11 +2,12 @@ const player = [];
 const boasEscolhas = ["bitcoin", "curso de ingles", "coda"];
 const escolhas = document.querySelectorAll("button");
 let pag = 0;
+const botoes = document.getElementById("ultima");
 
 function verificar_valor(lista) {
   let valor = true;
   lista.forEach((obj) => {
-    if (obj.valor === false) {
+    if (obj.valor == false) {
       valor = false;
     }
   });
@@ -17,4 +18,14 @@ function info_check(info) {
   let verif;
   info === boasEscolhas[pag] ? (verif = true) : (verif = false);
   player.push({ escolha: info, valor: verif });
+  pag++;
+  console.log(player);
+}
+
+function end_history() {
+  if (verificar_valor(player)) {
+    botoes.innerHTML = "Desde pequeno ...........";
+  } else {
+    botoes.innerHTML = "Se só fez merda .............";
+  }
 }
