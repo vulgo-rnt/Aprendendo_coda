f7cce0f6c3dc16d794f58e4fbf4271aa6eb82f3c
Renan.T <rtoniolo4@gmail.com>
2023-06-16 21:37:08 -0300

    ok

--- vscode exer/Portifolio/index.js
+++ vscode exer/Portifolio/index.js
@@ -1,8 +1,10 @@
 const upButton = document.getElementById("up");
+const sections = document.querySelectorAll("section");
 
 document.addEventListener("scroll", () => {
   if (document.scrollingElement.scrollTop > 80) {
     upButton.style.opacity = 1;
+    document.getElementById("mouse").remove();
   } else {
     upButton.style.opacity = 0;
   }
@@ -19,8 +21,21 @@ document.querySelectorAll(".button").forEach((element) => {
         document.scrollingElement.scrollTop = 0;
         break;
       case "Contact":
-        document.scrollingElement.scrollTop = 250;
+        document.scrollingElement.scrollTop = 5000;
+        break;
+      case "About":
+        document.scrollingElement.scrollTop = 590;
+        break;
+      case "Work":
+        document.scrollingElement.scrollTop = 1080;
         break;
     }
+  });
+});
+
+document.getElementById("color-mode").addEventListener("click", (event) => {
+  event.target.classList.toggle("dark_mode");
+  sections.forEach((element) => {
+    element.classList.toggle("dark_mode");
   });
 });
